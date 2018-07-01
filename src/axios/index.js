e03d9135c4efff9db4ee361262e2fc83a4ca7101
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-07-01 20:57:47 +0700

    Base Url Axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+// const url = 'http://localhost:3000'
+const url = 'http://35.185.189.173'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL:  url
 })
